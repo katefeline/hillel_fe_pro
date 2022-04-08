d9c05b0bb509ea1ef6bc3d841e7aa7a1d74aeb99
Kateryna Shadrina <katefeline@gmail.com>
2022-04-08 13:35:43 +0300

    for method used instead of do...while

--- ht10_chunk/script.js
+++ ht10_chunk/script.js
@@ -1,14 +1,21 @@
 const arr = [1,2,3,4,5,6,7,8,9,10];
 
 function chunk(arr, chunkSize) {
+    let arrayFullOfChunks = [];
     if (chunkSize === 0) {
         console.log('Error. You cannot use 0');
+    } else if (chunkSize === 1) {
+        console.log(arr);
     } else {
-        do {
-            console.log(arr.splice(0, chunkSize));
-        } while (arr.length > 0);
+        for (key in arr) {
+            arrayFullOfChunks.push(arr.splice(0, chunkSize));
+            if (arr.length < chunkSize) {
+                arrayFullOfChunks.push(arr);
+            }
+        }
+        console.log(arrayFullOfChunks);
     }
 
 }
 
-chunk(arr, 3);
+chunk(arr, 4);
