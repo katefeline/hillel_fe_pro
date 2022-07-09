f20f8bcb6de675c774cb9d16b6f7ef0eeddbc76d
Kateryna Shadrina <katefeline@gmail.com>
2022-07-09 20:37:39 +0300

    list with errors

--- ht19/script.js
+++ ht19/script.js
@@ -3,24 +3,42 @@ let elementId = 1;
 class ShoppingList {
     list = [];
 
-    constructor(title, author, maxCount) {
+    constructor(title, author, maxAmount) {
         this.title = title;
         this.author = author;
-        this.maxCount = maxCount;
+        this.maxAmount = maxAmount;
     }
 
-    addItem() {
+    addItem(title, count, unit) {
+        if (this.list.length >= this.maxAmount) {
+            throw new Error('You cannot add more items in the list!');
+        }
 
-    }
+        if (title === undefined || title === '') {
+            throw new Error(`${title} is blank`);
+        }
+        if (count === undefined || count === '') {
+            throw new Error(`${count} is blank`);
+        }
+        if (unit === undefined || unit === '') {
+            throw new Error(`${unit} is blank`);
+        }
 
-    removeItem() {
-        // if (this.list.find(elem) => elem.id === id ) {
-        //     this.list = this.list.filter((elem) => elem.id !== id)
-        // }
-    }
+        this.list.push(new ShoppingListElement(title, count, unit));
+        elementId++;
+
+
+    };
+
+    removeItem(id) {
+        if (!this.list.find((elem) => elem.id === +id)) {
+            throw new Error(`${id} cannot be found`);
+        }
+        this.list = this.list.filter((elem) => elem.id !== +id);
+    };
 }
 
-class ShoppingListItems {
+class ShoppingListElement {
     constructor(title, count, unit) {
         this.id = elementId;
         this.title = title;
@@ -28,3 +46,38 @@ class ShoppingListItems {
         this.unit = unit;
     }
 }
+
+let myShoppingList = new ShoppingList('List', 'Kate', '1');
+(() => {
+    try {
+        myShoppingList.addItem('milk', '1', 'lt');
+    } catch (ex) {
+        console.log(ex)
+    }
+
+    try {
+        myShoppingList.addItem('cheese', '0.5', 'kg');
+    } catch (ex) {
+        console.log(ex)
+    }
+
+    try {
+        myShoppingList.addItem('cat food', '15', 'canes');
+    } catch (ex) {
+        console.log(ex)
+    }
+
+    try {
+        myShoppingList.removeItem('7');
+    } catch (ex) {
+        console.log(ex)
+    }
+
+    try {
+        myShoppingList.addItem('book', '', '');
+    } catch (ex) {
+        console.log(ex)
+    } finally {
+        console.log(myShoppingList.list);
+    }
+})();
